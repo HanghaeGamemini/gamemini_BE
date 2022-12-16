d0169f3518d12a79ced0315432c6c92f5246a741
Youngju Jang <joj1043@nate.com>
2022-12-16 16:45:55 +0900

    기본셋팅

---
+++ src/main/java/com/hanghae/gamemini/GameminiApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.gamemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameminiApplication.class, args);
+	}
+
+}
