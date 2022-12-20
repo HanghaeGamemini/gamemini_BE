8e373e808dee182c9037328d86b48b1522e0b2bd
me010suye <108880977+addydfdf@users.noreply.github.com>
2022-12-20 16:55:58 +0900

    마이페이지-게시글,좋아요

--- src/main/java/com/hanghae/gamemini/service/MyPageService.java
+++ src/main/java/com/hanghae/gamemini/service/MyPageService.java
@@ -23,20 +23,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MyPageService {
 
-    public  final PostRepository postRepository;
-    public  final CommentRepository commentRepository;
+    public final PostRepository postRepository;
+    public final CommentRepository commentRepository;
 
-    public  final LikeRepository likeRepository;
+    public final LikeRepository likeRepository;
 
     //내가 작성한 게시글 찾기
-    public ResponseEntity <?> getMyPost(User user){
-       List<Post> posts = postRepository.findAllByUsername(user.getUsername());
+    public ResponseEntity<?> getMyPost(User user) {
+        List<Post> posts = postRepository.findAllByUsername(user.getUsername());
         List<PostResponseDto.AllPostResponseDto> allPostResponseDtos = new ArrayList<>();
 
-        if(posts.isEmpty()){
-            return new ResponseEntity<>(new PrivateResponseBody(CommonStatusCode.NO_COMMENT),HttpStatus.OK);
+        if (posts.isEmpty()) {
+            return new ResponseEntity<>(new PrivateResponseBody(CommonStatusCode.NO_COMMENT), HttpStatus.OK);
         }
-        for(Post post :posts) {
+        for (Post post : posts) {
             //post를 하나씩 하나씩 빼서 ENTITY로 리턴
             PostResponseDto.AllPostResponseDto allPostResponseDto = new PostResponseDto.AllPostResponseDto(post);
             allPostResponseDtos.add(allPostResponseDto);
@@ -45,24 +45,24 @@ public class MyPageService {
     }
 
 
-        //내가 좋아요한 게시글 불러오기
-        public ResponseEntity<PrivateResponseBody> getMyLikePost (User user){
-            List<Likes> likesList = likeRepository.findByUserId(user.getId()); //좋아요가 있는지 확인
+    //내가 좋아요한 게시글 불러오기
+    public ResponseEntity<PrivateResponseBody> getMyLikePost(User user) {
+        List<Likes> likesList = likeRepository.findByUserId(user.getId()); //좋아요가 있는지 확인
 
-            List<PostResponseDto.AllPostResponseDto> allPostResponseDtos = new ArrayList<>();
+        List<PostResponseDto.AllPostResponseDto> allPostResponseDtos = new ArrayList<>();
 
-            for (Likes likes : likesList) {
-                Long post_id = likes.getPost().getId(); //좋아요한 게시글의 아이디값 얻기
-                Post post = postRepository.findById(post_id).orElseThrow( //게시글이 있는지 확인
-                        () -> new RestApiException(CommonStatusCode.NO_ARTICLE)
-                );
-                //만약에 있으면
-                PostResponseDto.AllPostResponseDto allPostResponseDto1 = new PostResponseDto.AllPostResponseDto(post);
-               allPostResponseDtos.add(allPostResponseDto1);
+        for (Likes likes : likesList) {
+            Long post_id = likes.getPost().getId(); //좋아요한 게시글의 아이디값 얻기
+            Post post = postRepository.findById(post_id).orElseThrow( //게시글이 있는지 확인
+                    () -> new RestApiException(CommonStatusCode.NO_ARTICLE)
+            );
+            //만약에 있으면
+            PostResponseDto.AllPostResponseDto allPostResponseDto1 = new PostResponseDto.AllPostResponseDto(post);
+            allPostResponseDtos.add(allPostResponseDto1);
 
-            }
-            return new ResponseEntity<>(new PrivateResponseBody(CommonStatusCode.OK, allPostResponseDtos), HttpStatus.OK);
         }
+        return new ResponseEntity<>(new PrivateResponseBody(CommonStatusCode.OK, allPostResponseDtos), HttpStatus.OK);
+    }
 
 
     //내가 작성한 댓글 불러오기
@@ -82,6 +82,7 @@ public class MyPageService {
         }
         return new ResponseEntity<>(new PrivateResponseBody(CommonStatusCode.OK, commentResponseDtos), HttpStatus.OK);
     }
+}
 
 //    //프로필 변경하기
 //
